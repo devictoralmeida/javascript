d7b62f41a3b8b873313db890bc79f6860a122358
Victor Emmanuel Melo e Almeida <victoremmanuelmn@gmail.com>
2022-11-30 21:14:21 -0300

    aula 17 finalizada

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,3 +1,70 @@
+// METODO REPETIÇÃO DO WHILE
+
+function contar() {
+    var ini = document.getElementById ('txti')
+    var fim = document.getElementById ('txtf')
+    var passo = document.getElementById ('txtp')
+    var i = Number(ini.value)
+    var f = Number(fim.value)
+    var p = Number(passo.value)
+    var res = document.getElementById ('res')
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        alert('Por favor, preencha os dados corretamente!')
+    } else {
+        res.innerHTML = 'Contando...'
+        var c = i
+        if (i < f) {
+            do {
+                res.innerHTML += `${c} \u{1F449} `
+                c += p
+            } while (c <= f)
+        } else {
+            do {
+                res.innerHTML += `${c} \u{1F449} `
+                c -= p
+            } while (c >= f)
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
+
+/*
+
+ESTRUTURA DE REPETIÇÃO WHILE
+
+function contar() {
+    var ini = document.getElementById ('txti')
+    var fim = document.getElementById ('txtf')
+    var passo = document.getElementById ('txtp')
+    var i = Number(ini.value)
+    var f = Number(fim.value)
+    var p = Number(passo.value)
+    var res = document.getElementById ('res')
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        alert('Por favor, preencha os dados corretamente!')
+    } else {
+        res.innerHTML = 'Contando...'
+        var c = i
+        if (i < f) {
+            while (c <= f) {
+                res.innerHTML += `${c} \u{1F449} `
+                c += p
+            }
+        } else {
+            while (c >= f) {
+                res.innerHTML += `${c} \u{1F449} `
+                c -= p
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
+*/
+
+/*
+
+ESTRUTURA DE REPETIÇÃO FOR
+
 function contar() {
     var ini = document.getElementById('txti')
     var fim = document.getElementById('txtf')
@@ -25,6 +92,7 @@ function contar() {
     res.innerHTML += `\u{1F3C1}`
     }
 }
+*/
 
 
 
--- aula14ex/ex017.guanabara/script.js
+++ aula14ex/ex017.guanabara/script.js
@@ -1,29 +1,64 @@
+// USANDO ESTRUTURA DE REPETIÇÃO DO
 function gerar() {
     var num = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
     var n = Number(num.value)
-    var res1 = document.getElementById ('1')
-    var res2 = document.getElementById ('2')
-    var res3 = document.getElementById ('3')
-    var res4 = document.getElementById ('4')
-    var res5 = document.getElementById ('5')
-    var res6 = document.getElementById ('6')
-    var res7 = document.getElementById ('7')
-    var res8 = document.getElementById ('8')
-    var res9 = document.getElementById ('9')
-    var res10= document.getElementById ('10')
-
     if (num.value.length == 0) {
-        window.alert ('Por favor, digite um número!')
+        alert('Por favor, preencha um número acima!')
     } else {
-        res1.innerHTML = `${n} x 1 = ${n * 1}`
-        res2.innerHTML = `${n} x 2 = ${n * 2}`
-        res3.innerHTML = `${n} x 3 = ${n * 3}`
-        res4.innerHTML = `${n} x 4 = ${n * 4}`
-        res5.innerHTML = `${n} x 5 = ${n * 5}`
-        res6.innerHTML = `${n} x 6 = ${n * 6}`
-        res7.innerHTML = `${n} x 7 = ${n * 7}`
-        res8.innerHTML = `${n} x 8 = ${n * 8}`
-        res9.innerHTML = `${n} x 9 = ${n * 9}`
-        res10.innerHTML = `${n} x 10 = ${n * 10}`
+        var c = 1
+        tab.innerHTML = ''
+        do {
+            var item = document.createElement('option')
+            tab.appendChild(item)
+            item.innerHTML = `${n} x ${c} = ${n * c}`
+            c++
+        } while (c <= 10)
     }
 }
+
+/*
+
+USANDO ESTRUTURA DE REPETIÇÃO FOR
+
+function gerar () {
+    var num = document.getElementById('txtn')
+    var tab = document.getElementById ('seltab')
+    var n = Number(num.value)
+    if (num.value.length == 0) {
+        window.alert ('Por favor, preencha um número acima!')
+    } else {
+        tab.innerHTML = ''
+        for (var c = 1; c <= 10; c++) {
+            var item = document.createElement('option')
+            tab.appendChild(item)
+            item.innerHTML = `${n} x ${c} = ${n * c}`
+        }
+    }    
+}
+
+*/
+
+/*
+
+USANDO ESTRUTURA DE REPETIÇÃO WHILE
+
+function gerar () {
+    var num = document.getElementById ('txtn')
+    var tab = document.getElementById ('seltab')
+    var n = Number(num.value)
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        var c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            var item =  document.createElement ('option')
+            item.text = `${n} x ${c} = ${n * c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
+*/
